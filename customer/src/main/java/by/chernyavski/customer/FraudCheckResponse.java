ceafd91f3328c989965b48c6e5aa12f117852487
VladChernyavski <chernyavskivl@gmail.com>
2022-03-18 17:08:48 +0300

    fraud microservice

---
+++ customer/src/main/java/by/chernyavski/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package by.chernyavski.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/by/chernyavski/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package by.chernyavski.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
