8d5f9b4ebc3bef497fda6e99c3f2c935ea2b1922
VladChernyavski <chernyavskivl@gmail.com>
2022-03-25 17:09:56 +0300

    rabbitmq

--- notification/src/main/java/by/chernyavski/notification/NotificationApplication.java
+++ notification/src/main/java/by/chernyavski/notification/NotificationApplication.java
@@ -1,15 +1,36 @@
 package by.chernyavski.notification;
 
+import by.chernyavski.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "by.chernyavski.notification",
+                "by.chernyavski.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("Vat", 21),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey()
+//            );
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 
 }
